c40418d1d452b13ba6202ba1d51ff02985363da6
Hanyecz Marcell <marcellhanyecz@gmail.com>
2021-11-05 14:09:55 +0100

    repository CRUD methods implemented

---
+++ AQD1OI_HFT_2021221.Repository/BrandRepository.cs
@@ -0,0 +1,48 @@
+﻿using AQD1OI_HFT_2021221.Data;
+using AQD1OI_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AQD1OI_HFT_2021221.Repository
+{
+    class BrandRepository : IBrandRepository
+    {
+        CarDbContext DbContext;
+        public BrandRepository(CarDbContext DbContext)
+        {
+            this.DbContext = DbContext;
+        }
+        public void Create(Brand brand)
+        {
+            DbContext.Brands.Add(brand);
+            DbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            DbContext.Remove(Read(id));
+            DbContext.SaveChanges();
+        }
+
+        public Brand Read(int id)
+        {
+            return DbContext.Brands.FirstOrDefault(brand => brand.ID == id);
+        }
+
+        public IQueryable<Brand> ReadAll()
+        {
+            return DbContext.Brands;
+        }
+
+        public void Update(Brand brand)                 
+        {
+            var old = Read(brand.ID);
+            old.Name = brand.Name;
+            DbContext.SaveChanges();
+
+        }
+    }
+}
---
+++ AQD1OI_HFT_2021221.Repository/RentalRepository.cs
@@ -0,0 +1,49 @@
+﻿using AQD1OI_HFT_2021221.Data;
+using AQD1OI_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AQD1OI_HFT_2021221.Repository
+{
+    class RentalRepository : IRentalRepository
+    {
+        CarDbContext DbContext;
+        public RentalRepository(CarDbContext DbContext)
+        {
+            this.DbContext = DbContext;
+        }
+        public void Create(Rental rental)
+        {
+            DbContext.Rentals.Add(rental);
+            DbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            DbContext.Remove(Read(id));
+            DbContext.SaveChanges();
+        }
+
+        public Rental Read(int id)
+        {
+            return DbContext.Rentals.FirstOrDefault(r => r.ID == id);
+        }
+
+        public IQueryable<Rental> ReadAll()
+        {
+            return DbContext.Rentals;
+        }
+
+        public void Update(Rental rental)
+        {
+            var old = Read(rental.ID);
+            old.Date = rental.Date;
+            old.Renter = rental.Renter;
+            old.CarID = rental.CarID;
+            DbContext.SaveChanges();
+        }
+    }
+}
